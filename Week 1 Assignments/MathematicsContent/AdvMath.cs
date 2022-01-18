f0066b2d1925250dd5e6d0bb199d60e48453eb6a
Saimer <saimer.nieves@gmail.com>
2022-01-18 17:15:11 -0500

    Corey Please View MathematicsContents as Mathematics

---
+++ Week 1 Assignments/MathematicsContent/AdvMath.cs
@@ -0,0 +1,57 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+
+namespace Mathematics
+{
+    public class AdvMath
+    {
+
+        public double areaCalculator(double length, double width)
+        {
+
+            return length * width;
+
+        }
+
+        public double averageOfDoubleCalculator(List<double> listofDoubles)
+        {
+            var sizeOfList = listofDoubles.Count;
+            double sumOfList = 0;
+
+            foreach (double number in listofDoubles)
+            {
+                sumOfList += number;
+            }
+
+            
+            
+
+            return sumOfList / sizeOfList;
+
+
+        }
+
+        //Calculate Value Squared (Singular value multiplied by itself)
+        public double squareNumberCalculator(double numberToSquare)
+        {
+
+            return numberToSquare * numberToSquare;
+        }
+
+        //. Calculate Pythagorean Theorem (a2 + b2 = c2). ***HINT The third function should help you with that.
+        public double pythagoreanTheoremCalculator(double a, double b)
+        {
+
+
+            return (a * a) + (b * b);
+
+        }
+
+    }
+}
+
+
+
+
